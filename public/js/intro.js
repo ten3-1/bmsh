220e5d2c4ae5d6425823a8d5b1e7721dad2b961c
aureliefb <34569019+aureliefb@users.noreply.github.com>
2018-01-04 22:39:12 +0100

    modif landing page et css associe
    landing page ajout img et modif css associé

--- public/js/intro.js
+++ public/js/intro.js
@@ -1,10 +1,15 @@
 // images apparitions
 $(document).ready(function() {
     // apparition d'une image
-    $(".mshnb").delay( 600 ).fadeIn( 800 );
-    $(".mshnb").delay( 600 ).fadeOut( 600 );
-    $(".boutique").delay( 2000 ).fadeIn( 800 );
-    $(".boutique").delay( 2000 ).fadeOut( 600 );
+    $(".mshnb").delay( 800 ).fadeIn( 800 );
+    $(".boutique").delay( 1800 ).fadeIn( 800 );
+    $(".mains").delay( 2500 ).fadeIn( 1800);
+    $("video").delay( 2100 ).fadeIn( 800 );
+
+    $(".mshnb").delay( 1500 ).fadeOut( 800 );
+    $(".boutique").delay( 2100 ).fadeOut( 900 );
+    $(".mains").delay( 3500 ).fadeOut( 2500 );
+    $("video").delay( 2800 ) .fadeOut( 800 ); 
 });
 
 
@@ -14,6 +19,6 @@ xhr.open("GET", "./accueil", true);
 xhr.onload = function() {
     setInterval(function() {
     window.location = "./accueil"
-    }, 3000)
+    }, 20000)
 }
 xhr.send()
